3ce9c1206e628d2a2151646757d0d02726fedc2c
nikhilgalla <nikhilbhargav.galla@gmail.com>
2022-01-19 16:42:15 +0530

    updated

--- Deploy.sh
+++ Deploy.sh
@@ -1,4 +1,11 @@
 hello 
 guys thies contains deployment fiels
 hello friends  i M UPDATONG
+<<<<<<< HEAD
+=======
+hello again good moening
+hei friends good eving
+he;llo freidsvdghvsdbasdnsjnm,1909w82
+
+>>>>>>> a9a568d (updated)
 `
